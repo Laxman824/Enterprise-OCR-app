b559f41ea3d59cabeb4faa06db384a13818952fa
K Laxman <71651991+Laxman824@users.noreply.github.com>
2024-11-07 20:23:28 +0530

    v3

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+# src/__init__.py
+from . import processors
+from . import utils
+from . import templates
+
+# src/processors/__init__.py
+from .document_processor import EnterpriseDocumentProcessor
+
+# src/utils/__init__.py
+from .visualization import DocumentVisualizer
+from .validation import DocumentValidator
+
+# src/templates/__init__.py
+from .document_templates import DOCUMENT_TEMPLATES
